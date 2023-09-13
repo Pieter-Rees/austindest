ceca6b3ddb11a8443f89184bdddec1b2edf21896
Pieter Rees <pieter@pieterrees.nl>
2023-09-13 18:04:12 +0200

    only hover on lg

---
+++ src/app/components/sidenav.tsx
@@ -0,0 +1,97 @@
+import { Link, animateScroll as scroll } from "react-scroll";
+import Socials from "./socials";
+
+interface sideNavProps {
+  handleToggle: Function;
+  showSideNav: boolean;
+}
+
+export default function Sidenav(props: sideNavProps) {
+  return (
+    <nav
+      className={`block lg:hidden transition-all fixed left-0  top-0 h-full w-9/12 overflow-hidden  bg-black/90 backdrop-blur-md transform-gpu ${
+        props.showSideNav ? "" : "-translate-x-full"
+      }`}
+    >
+      <div>
+        <div className="flex flex-col justify-center w-full justify-end uppercase gap-8 mt-8">
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="landing"
+            spy={true}
+            smooth={true}
+            offset={-300}
+            className="text-3xl text-center cursor-pointer block mt-4 lg:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Home
+          </Link>
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="gigs"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            className="text-3xl text-center cursor-pointer block mt-4 md:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Gigs
+          </Link>
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="bio"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            className="text-3xl text-center cursor-pointer block mt-4 md:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Bio
+          </Link>
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="listen"
+            spy={true}
+            offset={-150}
+            smooth={true}
+            className="text-3xl text-center cursor-pointer block mt-4 md:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Listen
+          </Link>
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="watch"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            className="text-3xl text-center cursor-pointer block mt-4 md:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Watch
+          </Link>
+          <Link
+            activeClass="text-bubblegum hover:cursor-default"
+            to="contact"
+            spy={true}
+            offset={-300}
+            smooth={true}
+            className="text-3xl text-center cursor-pointer block mt-4 md:inline-block md:mt-0 hover:text-bubblegum mr-4"
+            duration={500}
+            onClick={props.handleToggle}
+          >
+            Contact
+          </Link>
+        </div>
+        <div className="mt-24">
+          <Socials />
+        </div>
+      </div>
+    </nav>
+  );
+}
