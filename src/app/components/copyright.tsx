4864efb3f57f06c199eafffff26e50602af787ce
Pieter Rees <pieter@pieterrees.nl>
2023-09-14 18:18:25 +0200

    lekker

--- src/app/components/copyright.tsx
+++ src/app/components/copyright.tsx
@@ -11,22 +11,21 @@ export default function Copyright() {
 
   return (
     <div className="relative z-3 grid lg:grid-cols-2 gap-4 bg-black backdrop-blur-md p-8">
+      <Socials />
+      <div className="block lg:hidden">
+        <hr />
+      </div>
       <div className=" flex flex-col items-center">
         <div
-          className="cursor-pointer mt-4 transition-all fill-white hover:fill-bubblegum ease-in-out"
+          className="cursor-pointer mt-4 lg:mt-0 transition-all fill-white hover:fill-bubblegum ease-in-out"
           onClick={() => scrollToTop()}
         >
           <Logo />
         </div>
 
-        <div className="text-center text-lg">
-          Copyright © {year} Austin dest
-        </div>
+        <div className="text-center 2xl:text-2xl ">Copyright © {year}</div>
+        <div className="text-center 2xl:text-2xl">Austin Dest</div>
       </div>
-      <div className="block lg:hidden">
-        <hr />
-      </div>
-      <Socials />
     </div>
   );
 }
