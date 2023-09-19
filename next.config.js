f0ca36218357270f7396f0db4420464992f99dd4
Pieter Rees <pieter@pieterrees.nl>
2023-09-19 20:37:56 +0200

    favicons

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    output: "export",
+    minimumCacheTTL: 60,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
